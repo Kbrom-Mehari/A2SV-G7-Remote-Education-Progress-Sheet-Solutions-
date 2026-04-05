96733e556b9b2c3c78ff4002d8f30d3b770cc425
kbrom <kibrommehari121@gmail.com>
2026-04-05 22:24:33 +0300

    palindrom linked list problem

---
+++ leetcode/PalindromeLinkedList.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        #find the middle
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        #reverse the second half(from middle - end)
+        prev = None
+        curr = slow
+        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        
+        #compare both halves (prev is head of the second half)
+        left = head
+        right = prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+
+
+
+
